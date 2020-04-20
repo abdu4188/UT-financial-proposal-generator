5e2be10f44a67a10d563cbcf2e4857b8e84d447a
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-04-20 12:35:00 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <Navbar/>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/layout/Navbar'
+import Footer from '@/components/layout/Footer'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="page-footer blue darken-3">
+        <div class="container">
+        </div>
+        <div class="footer-copyright">
+        <div class="container center">
+        © 2020 UT Solutions P.L.C
+        </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+
+}
+</script>
+
+<style>
+
+</style>
