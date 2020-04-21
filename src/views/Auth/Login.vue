161fadaea9a97aac614e2e90c73b46cdb292cd27
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-04-21 20:35:11 +0300

    connect login with firebase

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -5,20 +5,22 @@
             <div class="card-title center">
                 <h4 class="blue-text darken-3">Login</h4>
             </div>
-            <form class="">
+            <form @submit.prevent="login">
                 <div class="row col-sm-12">
                     <div class="col-sm-4"></div>
                     <div class="col-sm-4 center.align">
                         <div class="field input-field">
-                            <input type="email" name="email">
+                            <input v-model="email" type="email" name="email">
                             <label for="email">Email</label>
                         </div>
+                        <p class="red-text darken-3" v-if="emailFeedback">{{ emailFeedback }}</p>
                         <div class="field input-field">
-                            <input type="password" name="password">
+                            <input v-model="password" type="password" name="password">
                             <label for="password">Password:</label>
                         </div>
+                        <p class="red-text darken-3" v-if="passwordFeedback">{{ passwordFeedback }}</p>
                         <div class="center">
-                            <a class="waves-effect waves-light btn blue darken-3 white-text">Login</a><br><br>
+                            <button class="waves-effect waves-light btn blue darken-3 white-text">Login</button><br><br>
                             <a class="flat">Forgot password</a>
                         </div>
                     </div>
@@ -31,7 +33,49 @@
 </template>
 
 <script>
+import {auth} from '@/firebase/init'
+
 export default {
+name:'Login',
+    data(){
+        return{
+            email: null,
+            password: null,
+            emailFeedback: null,
+            passwordFeedback: null
+        }
+    },
+    created(){
+        auth.signOut().then()
+    },
+    methods: {
+        login(){
+            console.log("fun");
+            
+            this.$Progress.start()
+            if(this.email && this.password){
+                this.passwordFeedback = null
+                this.emailFeedback =  null
+                auth.signInWithEmailAndPassword(this.email, this.password).then(
+                    () => {
+                        this.$Progress.finish()
+                        this.$router.push({name: 'Home'})
+                    }
+                ).catch(err => {
+                    this.$Progress.fail()
+                    this.passwordFeedback = err.message
+                })
+            }
+            else if(this.email == null){
+                this.$Progress.fail()
+                this.emailFeedback = "Please enter an email address"
+            }
+            else{
+                this.$Progress.fail()
+                this.passwordFeedback = "Please enter your password"
+            }
+        },
+    }
 
 }
 </script>
